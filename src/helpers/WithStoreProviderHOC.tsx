52bcdd1b6577c7bfd7cad36005f7f7c5daec298f
tylerhal <tylerhhal@gmail.com>
2021-01-28 12:36:18 -0600

    feat: PR comments part 2

--- src/helpers/WithStoreProviderHOC.tsx
+++ src/helpers/WithStoreProviderHOC.tsx
@@ -1,5 +1,6 @@
-import { store } from '../renderer/store/store';
+import React from 'react';
 import { Provider } from 'react-redux';
+import { store } from '../renderer/store/store';
 
 export const withStoreProvider = (Component) => (props) => (
 	<Provider store={store}>
