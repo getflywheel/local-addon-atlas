2bd26a804f3d115ab401406383f3b4640e72a8bc
Andrew Botz <andrew.botz@getflywheel.com>
2021-01-31 21:20:42 -0600

    feat(LOC-2549): add headless overview row

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,8 +1,22 @@
 import path from 'path';
 import { HeadlessEnvironmentSelect } from './renderer/HeadlessEnvironmentSelect';
 import { withStoreProvider } from './helpers/WithStoreProviderHOC';
+import SiteOverviewRow from './renderer/SiteOverviewRow';
+import type { Site } from '@getflywheel/local';
 
 const stylesheetPath = path.resolve(__dirname, '../style.css');
+
+const nodeJSSiteOverviewRowHook = (hooks) => {
+	const SiteOverviewRowHOC = withStoreProvider(SiteOverviewRow);
+	if (global.localhostRouting) {
+		hooks.addContent('SiteInfoOverview_TableList', (site: Site) => {
+			const hasNodeJSHeadlessSite = site?.services?.nodejs?.ports?.NODEJS[0];
+			const nodeJSHeadlessLocalUrl = `localhost:${site?.services?.nodejs?.ports?.NODEJS[0]}`;
+
+			return (hasNodeJSHeadlessSite && <SiteOverviewRowHOC key={nodeJSHeadlessLocalUrl} localUrl={nodeJSHeadlessLocalUrl} />);
+		});
+	}
+};
 
 export default function (context) {
 	const { React, hooks } = context;
@@ -18,5 +32,7 @@ export default function (context) {
 	const NewSiteEnvironmentHOC = withStoreProvider(HeadlessEnvironmentSelect);
 
 	// Create the additional selection option to be displayed during site creation
-	hooks.addContent('NewSiteEnvironment_ EnvironmentDetails', ({ siteInfo }) => <NewSiteEnvironmentHOC siteInfo = { siteInfo } />);
+	hooks.addContent('NewSiteEnvironment_ EnvironmentDetails', ({ siteInfo }) => <NewSiteEnvironmentHOC siteInfo={siteInfo} />);
+
+	nodeJSSiteOverviewRowHook(hooks);
 }
