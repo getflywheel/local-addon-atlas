f4326886387a15b30af6628f1dc10dd27fe45f05
tylerhal <tylerhhal@gmail.com>
2021-02-12 09:30:20 -0600

    feat(LOC-2555): PR comments round 2

--- src/renderer/_browserWindows/terminal.ts
+++ src/renderer/_browserWindows/terminal.ts
@@ -2,9 +2,8 @@ import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { ipcRenderer } from 'electron';
 import { IPC_EVENTS } from '../../constants';
-import os from 'os';
 
-const term = new Terminal();
+const term = new Terminal({ convertEol: true });
 
 const termFitAddon = new FitAddon();
 
@@ -15,7 +14,7 @@ termFitAddon.fit();
 // listener for incoming data
 if (!ipcRenderer.listenerCount(IPC_EVENTS.WRITE_XTERM)) {
 	ipcRenderer.on(IPC_EVENTS.WRITE_XTERM, (event, data: string) => {
-		term.write(data.replace(/\n/g, '\n\r'));
+		term.write(data);
 	});
 }
 
