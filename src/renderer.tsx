f6b2776198196aae2afd27b2a96ff822edc680e9
Andrew Botz <andrew.botz@getflywheel.com>
2021-02-15 08:21:57 -0600

    feat(LOC-2554): Use site overview addon section

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,26 +1,36 @@
 import path from 'path';
 import { HeadlessEnvironmentSelect } from './renderer/HeadlessEnvironmentSelect';
 import { withStoreProvider } from './helpers/WithStoreProviderHOC';
-import SiteOverviewRow from './renderer/SiteOverview';
+import SiteOverviewAddonSection from './renderer/SiteOverviewAddonSection';
 import type { Site } from '@getflywheel/local';
 const stylesheetPath = path.resolve(__dirname, '../style.css');
 
-const nodeJSSiteOverviewRowHook = (hooks) => {
-	const SiteOverviewHOC = withStoreProvider(SiteOverviewRow);
-	if (global.localhostRouting) {
-		hooks.addContent('SiteInfoOverview_TableList', (site: Site, siteStatus: string) => {
-			const hasNodeJSHeadlessSite = site?.services?.nodejs?.ports?.NODEJS[0];
-			const nodeJSHeadlessLocalUrl = `localhost:${site?.services?.nodejs?.ports?.NODEJS[0]}`;
+const title = `Front-end Node.js`;
+const atlasDocsUrl = `http://developers.wpengine.com`;
 
-			return (hasNodeJSHeadlessSite
-				&& <SiteOverviewHOC
-					key={nodeJSHeadlessLocalUrl}
-					localUrl={nodeJSHeadlessLocalUrl}
-					siteStatus={siteStatus}
-				/>);
-		});
+const nodeJSSiteOverviewHook = (site: Site, siteStatus: string) => {
+	const SiteOverviewHOC = withStoreProvider(SiteOverviewAddonSection);
+	if (global.localhostRouting) {
+		const hasNodeJSHeadlessSite = site?.services?.nodejs?.ports?.NODEJS[0];
+		const nodeJSHeadlessLocalUrl = `localhost:${site?.services?.nodejs?.ports?.NODEJS[0]}`;
+
+		return (hasNodeJSHeadlessSite
+			&& <SiteOverviewHOC
+				key={nodeJSHeadlessLocalUrl}
+				localUrl={nodeJSHeadlessLocalUrl}
+				siteStatus={siteStatus}
+			/>);
 	}
 };
+
+const renderTooltip = () => (
+	<div className="SiteOverviewAddonSectionTooltip">
+		Learn more about WP Engine Headless framework.
+		<div className="SiteOverviewAddonSectionTooltipLink">
+			<a href={atlasDocsUrl}>GO TO DOCS</a>
+		</div>
+	</div>
+);
 
 export default function (context) {
 	const { React, hooks } = context;
@@ -38,5 +48,14 @@ export default function (context) {
 	// Create the additional selection option to be displayed during site creation
 	hooks.addContent('NewSiteEnvironment_EnvironmentDetails', ({ siteInfo }) => <NewSiteEnvironmentHOC siteInfo={siteInfo} />);
 
-	nodeJSSiteOverviewRowHook(hooks);
+	hooks.addFilter('SiteInfoOverview_Addon_Section', (content, site: Site, siteStatus: string) => {
+
+		const nodeJSSection = {
+			title,
+			tooltip: renderTooltip(),
+			component: (nodeJSSiteOverviewHook(site, siteStatus)),
+		};
+		content.push(nodeJSSection);
+		return content;
+	});
 }
--- src/renderer/SiteOverviewAddonSection.tsx
+++ src/renderer/SiteOverviewAddonSection.tsx
@@ -1,45 +1,21 @@
 import React from 'react';
-import { TableListRow, TextButton } from '@getflywheel/local-components';
-import { SITE_STATUS } from '../constants';
+import { TableList, TableListRow, TextButton } from '@getflywheel/local-components';
 
 interface Props {
 	localUrl: string;
 	siteStatus: string;
 }
 
-const getSiteStatus = siteStatus => {
-	const STATUS = {
-		STOPPING: 'Stopping',
-		STOPPED: 'Stopped',
-		STARTING: 'Starting',
-		STARTED: 'Started',
-	};
-
-	switch (siteStatus) {
-		case SITE_STATUS.STOPPING:
-			return STATUS.STOPPING;
-		case SITE_STATUS.HALTED:
-			return STATUS.STOPPED;
-		case SITE_STATUS.RUNNING:
-			return STATUS.STARTED;
-		case SITE_STATUS.STARTING:
-			return STATUS.STARTING;
-		default:
-			return STATUS.STOPPED;
-	}
-};
-
 const showTerminalOutput = () => console.log('COMING SOON =)');
-
 
 const SiteOverview = (props: Props) => {
 	const { localUrl, siteStatus } = props;
 
 	return (
-		<>
+		<TableList>
 			<TableListRow label="Status" selectable>
 				<div style={{ flex: 1, display: 'flex', alignContent: 'center' }}>
-					<p>{getSiteStatus(siteStatus)}</p>
+					<p style={{ textTransform: 'capitalize' }}>{siteStatus}</p>
 					<TextButton size="tiny" onClick={() => showTerminalOutput()}>
 						Show Output
 					</TextButton>
@@ -52,7 +28,7 @@ const SiteOverview = (props: Props) => {
 				<a href={`http://${localUrl}`}>{localUrl}</a>
 
 			</TableListRow>
-		</>
+		</TableList>
 	);
 };
 
