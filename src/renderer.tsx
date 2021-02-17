5c206d1796b4326a04e15fdc6e3ca35cef3b30f1
tylerhal <tylerhhal@gmail.com>
2021-02-17 17:09:22 -0600

    refactor(LOC-2652): remove redux and update local components

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,20 +1,18 @@
 import path from 'path';
 import { HeadlessEnvironmentSelect } from './renderer/HeadlessEnvironmentSelect';
-import { withStoreProvider } from './helpers/WithStoreProviderHOC';
 import SiteOverviewAddonSection from './renderer/SiteOverviewAddonSection';
 import type { Site } from '@getflywheel/local';
 const stylesheetPath = path.resolve(__dirname, '../style.css');
 
 const title = `Front-end Node.js`;
-const atlasDocsUrl = `http://developers.wpengine.com`;
+export const atlasDocsUrl = `https://developers.wpengine.com`;
 
 const nodeJSSiteOverviewHook = (site: Site, siteStatus: string) => {
-	const SiteOverviewHOC = withStoreProvider(SiteOverviewAddonSection);
 	const hasNodeJSHeadlessSite = site?.services?.nodejs?.ports?.NODEJS[0];
 	const nodeJSHeadlessLocalUrl = `localhost:${site?.services?.nodejs?.ports?.NODEJS[0]}`;
 
 	return (hasNodeJSHeadlessSite
-		&& <SiteOverviewHOC
+		&& <SiteOverviewAddonSection
 			key={nodeJSHeadlessLocalUrl}
 			localUrl={nodeJSHeadlessLocalUrl}
 			siteStatus={siteStatus}
@@ -42,10 +40,8 @@ export default function (context) {
 		/>
 	));
 
-	const NewSiteEnvironmentHOC = withStoreProvider(HeadlessEnvironmentSelect);
-
 	// Create the additional selection option to be displayed during site creation
-	hooks.addContent('NewSiteEnvironment_EnvironmentDetails', ({ siteInfo }) => <NewSiteEnvironmentHOC siteInfo={siteInfo} />);
+	hooks.addContent('NewSiteEnvironment_EnvironmentDetails', () => <HeadlessEnvironmentSelect />);
 
 	hooks.addFilter('SiteInfoOverview_Addon_Section', (content, site: Site, siteStatus: string) => {
 
