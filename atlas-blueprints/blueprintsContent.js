bbb896ff82273c54e6819d20d0f9c83669cc6a83
Nick Cernis <nick@cern.is>
2023-01-26 12:48:13 +0100

    chore: update blueprint copy

--- atlas-blueprints/blueprintsContent.js
+++ atlas-blueprints/blueprintsContent.js
@@ -10,7 +10,7 @@ const atlasBlueprints = [
 		title: 'Portfolio Blueprint',
 		byline: 'Headless WordPress',
 		excerpt:
-			'Show off your craft and share your thoughts with this Blueprint. Includes pages to list your projects, and also a blog.',
+			'Select this Blueprint to start building your new portfolio. Get started with our existing theme as you focus on the content of your portfolio.',
 		previewHref:
 			'https://wpeng.in/atlas-blueprint-portfolio',
 		repoHref: 'https://github.com/wpengine/atlas-blueprint-portfolio',
@@ -21,10 +21,10 @@ const atlasBlueprints = [
 			__dirname,
 			'../atlas-blueprints/ecommerce-blueprint.png',
 		),
-		title: 'Ecommerce Blueprint',
+		title: 'BigCommerce Blueprint',
 		byline: 'Headless WordPress',
 		excerpt:
-			'Build your headless store with WordPress content and BigCommerce product data. Install everything you need to get started quickly, including product and navigation pages.',
+			'Build your headless store with WordPress content and BigCommerce product data. Install everything you need to get started quickly, including product and navigation pages. <br><strong><a href="https://developers.wpengine.com/docs/atlas-commerce-connector/introduction">Set up BigCommerce</a></strong>.',
 		previewHref:
 			'https://atlascommerce.wpengine.com/',
 		repoHref: 'https://github.com/wpengine/atlas-commerce-blueprint',
