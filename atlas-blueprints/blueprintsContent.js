4d97b01bfc5c0f9cac90fe4886518ed405311295
Austin Redick <austin.redick@wpengine.com>
2023-02-28 07:56:12 -0800

    chore: change order, update titles

--- atlas-blueprints/blueprintsContent.js
+++ atlas-blueprints/blueprintsContent.js
@@ -2,12 +2,26 @@ import path from 'path';
 
 const atlasBlueprints = [
 	{
+		id: 'faust-blueprint-atlas-1',
+		thumbnail: path.resolve(
+			__dirname,
+			'../atlas-blueprints/faust-boilerplate-blueprint.png',
+		),
+		title: 'Faust.js Scaffolding',
+		byline: 'Headless WordPress',
+		excerpt:
+			'Start a new Faust.js site from the beginning and craft it to your project. This blueprint is a basic project scaffold designed to get experienced developers moving quickly on new Faust.js projects.',
+		previewHref:
+			'https://wpeng.in/faust-scaffold',
+		repoHref: 'https://github.com/wpengine/faust-scaffold',
+	},
+	{
 		id: 'portfolio-blueprint-atlas-1',
 		thumbnail: path.resolve(
 			__dirname,
 			'../atlas-blueprints/portfolio-blueprint.png',
 		),
-		title: 'Portfolio Blueprint',
+		title: 'Faust.js Portfolio Blueprint',
 		byline: 'Headless WordPress',
 		excerpt:
 			'Select this Blueprint to start building your new portfolio. Get started with our existing theme as you focus on the content of your portfolio.',
@@ -28,20 +42,6 @@ const atlasBlueprints = [
 		previewHref:
 			'https://atlascommerce.wpengine.com/',
 		repoHref: 'https://github.com/wpengine/atlas-commerce-blueprint',
-	},
-	{
-		id: 'faust-blueprint-atlas-1',
-		thumbnail: path.resolve(
-			__dirname,
-			'../atlas-blueprints/faust-boilerplate-blueprint.png',
-		),
-		title: 'Faust Scaffold Blueprint',
-		byline: 'Headless WordPress',
-		excerpt:
-			'Start a new Faust.js site from the beginning and craft it to your project. This blueprint is a basic project scaffold designed to get experienced developers moving quickly on new Faust.js projects.',
-		previewHref:
-			'https://wpeng.in/faust-scaffold',
-		repoHref: 'https://github.com/wpengine/faust-scaffold',
 	},
 ];
 
